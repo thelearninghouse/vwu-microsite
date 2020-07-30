48c2c9339f34ddf20efbc1f9bc12b2c9b336fdf6
gmurthywiley <65417422+gmurthywiley@users.noreply.github.com>
2020-07-30 14:48:43 +0530

    change(Content) : Changes related to ticket MTIK-1144
    Changes related to MTIK-1144: Remove ePacket OLC References.

--- wp-content/themes/vwu-microsite/page-thank-you.php
+++ wp-content/themes/vwu-microsite/page-thank-you.php
@@ -5,12 +5,8 @@
 			<main class="main-content cf" role="main" itemscope="" itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 				<article style="max-width:690px;" class="cf post-124 page type-page status-draft hentry" role="article" itemscope="" itemtype="http://schema.org/BlogPosting">
 					<header class="page-header">
-						<h1 class="page-header__title" itemprop="headline">Thank You</h1>
+						<h1 class="page-header__title" itemprop="headline">Thank You!</h1>
 					</header> 
-					<section class="page-content cf" itemprop="articleBody">
-						<p>You’re one step closer to accomplishing your educational goals.</p>
-<p>We have received your information request and you will be contacted shortly by an enrollment counselor. If you have any questions, take a moment to write them down and ask when we call. We look forward to speaking with you.</p>
-					</section> 
 				</article>	
 			</main>
 		</div>
